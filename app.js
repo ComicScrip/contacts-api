cbab3c755fb0be62d59c91cea01f5f0d194c7b4c
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2020-12-13 12:22:40 +0100

    tmp

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ const corsOptions = {
   origin: (origin, callback) => {
     console.log('origin : ', origin);
     console.log('allowed', allowedOrigins);
-    if (allowedOrigins.indexOf(origin) !== -1) {
+    if (origin === undefined || allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
