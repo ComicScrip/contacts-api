f57939b4d787c6e26f6b0542b37a7510f038ec6f
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2020-12-13 12:58:54 +0100

    tmp

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,6 @@ const authController = require('../controllers/auth.js');
 const requireRequestBody = require('../middlewares/requireRequestBody.js');
 
 router.post('/login', requireRequestBody, asyncHandler(authController.login));
-router.get('/logout', requireRequestBody, asyncHandler(authController.logout));
+router.get('/logout', asyncHandler(authController.logout));
 
 module.exports = router;
